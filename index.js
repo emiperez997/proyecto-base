ba70a3c2092a1cc37683bd1b49705378dc13f607
Emiliano Perez <emi.perez997@gmail.com>
2023-12-19 22:52:57 -0300

    [ADD] Aggregation

---
+++ index.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import orderModel from "./models/order";
+
+const environment = async () => {
+  await mongoose.connect("mongodb://localhost:27017/pizza");
+
+  // Insert Many
+  const result = await orderModel.insertMany([
+    {
+      name: "Pepperoni",
+      size: "small",
+      price: 19,
+      quantity: 10,
+      date: "2021-05-01",
+    },
+    {
+      name: "Pepperoni",
+      size: "medium",
+      price: 20,
+      quantity: 20,
+      date: "2021-05-02",
+    },
+    {
+      name: "Pepperoni",
+      size: "large",
+      price: 21,
+      quantity: 30,
+      date: "2021-05-03",
+    },
+    {
+      name: "Cheese",
+      size: "small",
+      price: 12,
+      quantity: 15,
+      date: "2021-05-04",
+    },
+    {
+      name: "Cheese",
+      size: "medium",
+      price: 13,
+      quantity: 50,
+      date: "2021-05-05",
+    },
+    {
+      name: "Cheese",
+      size: "large",
+      price: 14,
+      quantity: 10,
+      date: "2021-05-06",
+    },
+    {
+      name: "Vegan",
+      size: "small",
+      price: 17,
+      quantity: 10,
+      date: "2021-05-07",
+    },
+    {
+      name: "Vegan",
+      size: "medium",
+      price: 18,
+      quantity: 10,
+      date: "2021-05-08",
+    },
+  ]);
+  console.log(result);
+
+  // Find
+  const orders = await orderModel.find();
+  console.log(result);
+};
--- src/routes/users.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { userModel } from "../models/user.model.js";
-
-const router = Router();
-
-export default router;
